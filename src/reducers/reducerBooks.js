903d964ca0ede6d43e77486b85f31ecbd601d6bc
AngieYeh <angieyeh24@gmail.com>
2018-07-12 16:37:21 +0000

    created reducer for book list

---
+++ src/reducers/reducerBooks.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Javascript: The Good Parts', pages: 300 },
+        {title: 'You Don\'t know Javascript', pages: 180},
+        {title: 'Eloquent Ruby', pages: 200},
+        {title: 'Web Scalability', pages: 120},
+    ];
+}
