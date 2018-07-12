10488195ffb4787d1fca393b65e7589fa4a56417
AngieYeh <angieyeh24@gmail.com>
2018-07-12 16:35:45 +0000

    set up parent component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import BookList from '../containers/bookList.js';
+import BookDetail from '../containers/bookDetails.js';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <BookList />
+                <BookDetail />
+            </div>
+        );
+    }
 }
